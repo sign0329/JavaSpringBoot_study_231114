5e9a73ec9cc042ebd7b44a04d93aa0b847dfd6b2
sign0329@gmail.com <https://github.com/sign0329/tb-2023-10-25>
2023-11-14 10:05:08 +0900

    스프링 부트 초기 환경 설정

---
+++ src/main/java/com/example/springboot_231114/Springboot231114Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_231114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot231114Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot231114Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_231114/Springboot231114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_231114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot231114ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
