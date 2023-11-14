61da4368144f792f2be5953b06bfe8c0d6295027
sign0329@gmail.com <https://github.com/sign0329/tb-2023-10-25>
2023-11-14 11:05:51 +0900

    cal3, cal4 구현

--- src/main/java/com/example/springboot_231114/HomeController.java
+++ src/main/java/com/example/springboot_231114/HomeController.java
@@ -2,6 +2,7 @@ package com.example.springboot_231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -30,4 +31,26 @@ public class HomeController {
 
         return "a:" + a + ", b:" + b;
     }
+    @GetMapping("/calc3")
+    @ResponseBody
+    String showCalc3(
+        @RequestParam(defaultValue = "0") int a,
+        @RequestParam(defaultValue = "0") int b ) {
+        return "계산기: %d".formatted(a + b);
+    }
+    @GetMapping("/calc4")
+    @ResponseBody
+    String showCalc4(
+            @RequestParam(defaultValue = "0") double a,
+            @RequestParam(defaultValue = "0") double b ) {
+        return "계산기: %f".formatted(a + b);
+    }
+    @GetMapping("/calc5")
+    @ResponseBody
+    String showCalc5(
+            @RequestParam(defaultValue = "-") String a,
+            @RequestParam(defaultValue = "-") String b ) {
+        return "계산기: %s".formatted(a + b);
+    }
 }
+
